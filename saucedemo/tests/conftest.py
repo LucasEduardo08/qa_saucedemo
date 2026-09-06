869231869de3ba9952a553da58936267b6b36ed5
Lucas Eduardo <lept.eng21@uea.edu.br>
2026-09-06 14:49:07 -0400

    build: dockerize test automatio project

--- saucedemo/tests/conftest.py
+++ saucedemo/tests/conftest.py
@@ -1,23 +1,20 @@
 import pytest
 from playwright.sync_api import sync_playwright, expect
-from src.pages.login_page import LoginPage
 from src.pages.home import HomePage
 from src.pages.product_page import ProductPage
+from src.pages.cart_page import CartPage
+from src.pages.checkout_page import CheckoutPage
 
 @pytest.fixture(scope="function")
 def page():
     with sync_playwright() as p:
         # headless=False permite ver o navegador abrindo na sua tela
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
         yield page
         context.close()
         browser.close()
-
-@pytest.fixture
-def login_page(page):
-    return LoginPage(page)
 
 @pytest.fixture
 def home_page(page):
@@ -26,3 +23,11 @@ def home_page(page):
 @pytest.fixture
 def product_page(page):
     return ProductPage(page)
+
+@pytest.fixture
+def cart_page(page):
+    return CartPage(page)
+
+@pytest.fixture
+def checkout_page(page):
+    return CheckoutPage(page)
